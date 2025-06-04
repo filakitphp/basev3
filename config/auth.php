ae5b6ad3168403a36b7a47f2abc561ff95164be8
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-04 07:50:06 -0300

    add 'admins' and 'password_reset_tokens' to auth configuration, update `expire` and `throttle` values.

--- config/auth.php
+++ config/auth.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Admin;
 use App\Models\User;
 
 return [
@@ -96,12 +95,6 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 60,
-            'throttle' => 60,
-        ],
-        'admins' => [
-            'provider' => 'admins',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
